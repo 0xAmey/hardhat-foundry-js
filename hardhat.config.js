6771b7ecbb18b4f806425fd6fcfb8e70b30fa1cd
Amey <96624879+0xamey@users.noreply.github.com>
2022-09-30 12:15:18 +0530

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,39 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("fs")
+require("hardhat-preprocessor")
+function getRemappings() {
+    return fs
+        .readFileSync("remappings.txt", "utf8")
+        .split("\n")
+        .filter(Boolean) // remove empty lines
+        .map((line) => line.trim().split("="))
+}
+
+module.exports = {
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.17",
+            },
+        ],
+    },
+    preprocess: {
+        eachLine: (hre) => ({
+            transform: (line) => {
+                if (line.match(/^\s*import /i)) {
+                    for (const [from, to] of getRemappings()) {
+                        if (line.includes(from)) {
+                            line = line.replace(from, to)
+                            break
+                        }
+                    }
+                }
+                return line
+            },
+        }),
+    },
+    paths: {
+        sources: "./src",
+        cache: "./cache_hardhat",
+    },
+}
