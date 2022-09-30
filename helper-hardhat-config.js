f2c002c980d0c2306cabd0c7429680dc757b5931
Amey <96624879+0xamey@users.noreply.github.com>
2022-09-30 14:51:54 +0530

    Create helper-hardhat-config.js

---
+++ helper-hardhat-config.js
@@ -0,0 +1,14 @@
+const networkConfig = {
+    31337: {
+        
+    },
+    5: {
+        name: "goerli",
+        
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+
+module.exports = {networkConfig, developmentChains}
